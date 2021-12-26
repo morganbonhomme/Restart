4a125b3d0dd66213a4a7c299ee5625e5f2a62562
morganbonhomme <mbonhomme@lucca.fr>
2021-12-26 16:05:54 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Morgan Bonhomme on 26/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
